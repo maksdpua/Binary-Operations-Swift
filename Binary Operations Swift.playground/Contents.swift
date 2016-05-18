81557627df3dacfae50fed572592bc67e380694a
Maks <maksdpua@gmail.com>
2016-05-18 15:12:18 +0300

    Start

---
+++ Binary Operations Swift.playground/Contents.swift
@@ -0,0 +1,25 @@
+
+
+extension UInt8 {
+    
+    func binary() -> String {
+        var result = ""
+        for i in 0..<8 {
+            let mask = 1 << i
+            let set = Int(self) & mask != 0
+            result = (set ? "1" : "0") + result
+        }
+        return result
+    }
+}
+
+var a: UInt8 = 57
+a.binary()
+
+a = 0b00111001
+
+a = a + 2
+a.binary()
+a = a + 3
+a.binary()
+
